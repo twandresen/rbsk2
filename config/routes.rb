e743500779171aa9fcd999a2cc96dcf555547b29
Lisa Ramirez <lmframirez@gmail.com>
2019-05-01 02:22:56 +0000

    additional practice areas pages

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,15 @@ Rails.application.routes.draw do
       get "new-adults" => "practices#new-adults", as: :new_adults
       get "seniors" => "practices#seniors", as: :seniors
       get "service-members" => "practices#service-members", as: :service_members
+      get "civil-litigation" => "practices#civil-litigation", as: :civil_litigation
+      get "estate-planning" => "practices#estate-planning", as: :estate_planning
+      get "fiduciary-services" => "practices#fiduciary-services", as: :fiduciary_services
+      get "guardianship" => "practices#guardianship", as: :guardianship
+      get "mental-health" => "practices#mental-health", as: :mental_health
+      get "personal-injury" => "practices#personal-injury", as: :personal_injury
+      get "probate-litigation" => "practices#probate-litigation", as: :probate_litigation
+      get "real-estate" => "practices#real-estate", as: :real_estate
+      get "trusts" => "practices#trusts", as: :trusts
 		end
 	end
 
